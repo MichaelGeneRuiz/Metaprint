b630ef6e207a92d4e649c50a95d5acbb238fc221
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2023-03-19 12:22:09 -0400

    Added Frontend Login Functionality + Backend Basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import {Route, Routes, Navigate} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Homepage from "./components/pages/Homepage";
+import LoginPage from "./components/pages/LoginPage";
+import PageNotFound from "./components/pages/PageNotFound";
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Homepage/>}/>
-      <Route path="/test" element={<div>Test Page!</div>}/>
+      <Route exact path="/" element={<Homepage />} />
+      <Route path="/Login" element={<LoginPage />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
