ea4efa9af788240f7a467acf2809cc1f31c841c2
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2023-03-19 12:22:09 -0400

    Added Frontend Login Functionality + Backend Basics

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -5,27 +5,10 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
 function Homepage() {
-  const [loginData, setLoginData] = useState("N/A");
   const [connectionData, setConnectionData] = useState(
     "Not Connected to Backend"
   );
   const navigate = useNavigate();
-
-  async function login() {
-    const res = await fetch("/login", {
-      method: "POST",
-      body: JSON.stringify({
-        stuff: "stuff",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await res.json();
-
-    setLoginData(data.message);
-  }
 
   async function getHome() {
     const res = await fetch("/home");
@@ -41,7 +24,7 @@ function Homepage() {
   function testButtonHandler(e) {
     e.preventDefault();
 
-    navigate("/test");
+    navigate("/login");
   }
 
   useEffect(() => {
@@ -49,13 +32,10 @@ function Homepage() {
   }, []);
 
   return (
-    <Container style={{ "text-align": "center" }}>
+    <Container style={{ textAlign: "center" }}>
       <div>Database Connection Test: {connectionData}</div>
       <hr />
-      <div>Success Message: {loginData}</div>
-      <Button onClick={login}>Click Me to Login</Button>
-      <hr />
-      <Button onClick={testButtonHandler}>View Test Page</Button>
+      <Button onClick={testButtonHandler}>View Login Page</Button>
     </Container>
   );
 }
