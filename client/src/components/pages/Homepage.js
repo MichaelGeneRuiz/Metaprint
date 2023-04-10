bcf3cec1d6d38863f61f255817558b94fc259fea
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2023-04-10 12:18:19 -0400

    Added NavBar, Added Aggregate Page, Removed Test Navigation

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -1,16 +1,8 @@
-import { useState, useEffect, useCallback, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-
-import Button from "react-bootstrap/Button";
+import { useState, useEffect, useCallback } from "react";
 import Container from "react-bootstrap/Container";
-
-import AuthContext from "../../context/AuthContext";
 
 function Homepage() {
   const [numUsers, setNumUsers] = useState("Not Connected to Backend");
-  const navigate = useNavigate();
-
-  const authCtx = useContext(AuthContext);
 
   const getHome = useCallback(async () => {
     const res = await fetch("/home");
@@ -23,48 +15,14 @@ function Homepage() {
     setNumUsers(`${data.users} users in the database.`);
   }, []);
 
-  function navigateLogin(e) {
-    e.preventDefault();
-
-    navigate("/login");
-  }
-
-  function navigateProfile(e) {
-    e.preventDefault();
-
-    navigate("/profile");
-  }
-
   useEffect(() => {
     getHome();
   }, [getHome]);
 
   return (
     <Container style={{ textAlign: "center" }}>
+      <div style={{fontSize: 48, fontWeight: "bold"}}>This is Metaprint!</div>
       <div>{numUsers}</div>
-      <hr />
-      {!authCtx.isLoggedIn && (
-        <Button onClick={navigateLogin}>View Login Page</Button>
-      )}
-      {authCtx.isLoggedIn && (
-        <div>
-          <p>You are currently logged in as {authCtx.email}</p>
-          <Button
-            onClick={() => {
-              authCtx.logout();
-              navigate(0);
-            }}
-          >
-            Logout
-          </Button>
-        </div>
-      )}
-      {authCtx.isLoggedIn && (
-        <div>
-          <p>Go to your profile:</p>
-          <Button onClick={navigateProfile}>Profile</Button>
-        </div>
-      )}
     </Container>
   );
 }
