c5aedb9dc3a352ada90551fec886df4243f8d623
MichaelGeneRuiz <michael.gene.ruiz@gmail.com>
2023-04-14 11:22:01 -0400

    Added Range Option for Aggregate Data, Removed Unnecessary User Activity Functions

--- client/src/components/aggregate/HistoricalEmissions.js
+++ client/src/components/aggregate/HistoricalEmissions.js
@@ -1,17 +1,27 @@
 import { useState, useContext } from "react";
 
 import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 import AuthContext from "../../context/AuthContext";
 
+import classes from "./HistoricalEmissions.module.css";
+
 function HistoricalEmissions() {
-  const [historicalEmissionData, setHistoricalEmissionData] = useState({});
-  const [buttonPressed, setButtonPressed] = useState(false);
+  const [historicalPresetData, setHistoricalPresetData] = useState({});
+  const [historicalRangeData, setHistoricalRangeData] = useState(0);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [presetButtonPressed, setPresetButtonPressed] = useState(false);
+  const [rangeButtonPressed, setRangeButtonPressed] = useState(false);
 
   const authCtx = useContext(AuthContext);
 
-  async function getHistoricalData(preset, preset_type) {
+  async function getHistoricalPresetData(preset, preset_type) {
     try {
       const res = await fetch("/viewHistoricalAggregateFootprint", {
         method: "POST",
@@ -28,31 +38,117 @@ function HistoricalEmissions() {
         throw new Error(data.message);
       }
 
-      setHistoricalEmissionData(data.data);
-      setButtonPressed(true);
+      setHistoricalPresetData(data.data);
+      setPresetButtonPressed(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function getHistoricalRangeData(preset) {
+    try {
+      const res = await fetch("/viewHistoricalAggregateFootprint", {
+        method: "POST",
+        body: JSON.stringify({
+          preset,
+          date_start: startDate,
+          date_end: endDate,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer: ${authCtx.token}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      setHistoricalRangeData(data.data);
+      setRangeButtonPressed(true);
     } catch (error) {
       console.log(error.message);
     }
   }
 
   return (
-    <Container>
-      <div>View historical data:</div>
-      <Button onClick={() => getHistoricalData(true, "day")}>Daily</Button>
-      <Button onClick={() => getHistoricalData(true, "week")}>Weekly</Button>
-      {Object.keys(historicalEmissionData).length > 0 && (
+    <Container className={classes.container}>
+      <div>Daily Breakdown of Emissions:</div>
+      <Row>
+        <Col>
+          <Button
+            className={classes.button}
+            onClick={() => getHistoricalPresetData(true, "day")}
+          >
+            Today
+          </Button>
+        </Col>
+        <Col>
+          <Button
+            className={classes.button}
+            onClick={() => getHistoricalPresetData(true, "week")}
+          >
+            Past Week
+          </Button>
+        </Col>
+      </Row>
+
+      {Object.keys(historicalPresetData).length > 0 && (
         <div>
-          {Object.keys(historicalEmissionData).map((key) => (
+          {Object.keys(historicalPresetData).map((key) => (
             <div key={key}>
               <h2>
-                {key}: {historicalEmissionData[key]}
+                {key}: {historicalPresetData[key]}
               </h2>
             </div>
           ))}
         </div>
       )}
-      {Object.keys(historicalEmissionData).length === 0 && buttonPressed && (
-        <div>No historical emission data found.</div>
+
+      {Object.keys(historicalPresetData).length === 0 &&
+        presetButtonPressed && <div>No preset emission data found.</div>}
+
+      <div>Total emissions from the selected range:</div>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          getHistoricalRangeData(false);
+        }}
+      >
+        <Row>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicFromDate">
+              <Form.Label>From</Form.Label>
+              <Form.Control
+                type="date"
+                required
+                onChange={(e) => setStartDate(e.target.value)}
+                value={startDate}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group className="mb-3" controlId="formBasicToDate">
+              <Form.Label>To</Form.Label>
+              <Form.Control
+                type="date"
+                required
+                onChange={(e) => setEndDate(e.target.value)}
+                value={endDate}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Button type="submit">Submit</Button>
+      </Form>
+      {rangeButtonPressed && (
+        <div>
+          From {new Date(startDate + "T00:00").toDateString()} to{" "}
+          {new Date(endDate + "T00:00").toDateString()}, Metaprint users emitted{" "}
+          {historicalRangeData} (fake unit) emissions.
+        </div>
       )}
     </Container>
   );
